8db6157bad84d8e2666d5325a190ca15098efb56
André Stanlley <andre.v@lyncas.net>
2022-05-06 17:14:01 -0300

    Liberação dos cors

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,12 @@ import express from 'express'
 import api from './Routes/api'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.use(morgan('tiny'))
